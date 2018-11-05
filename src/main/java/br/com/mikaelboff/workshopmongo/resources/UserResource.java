21ad191ef2a178aa46267604aaf816a917073a80
Mikael Boff <mikaelboff1@gmail.com>
2018-11-05 00:23:43 -0200

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/br/com/mikaelboff/workshopmongo/resources/UserResource.java
@@ -0,0 +1,25 @@
+package br.com.mikaelboff.workshopmongo.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.mikaelboff.workshopmongo.domain.User;
+import br.com.mikaelboff.workshopmongo.services.UserService;
+
+@RestController
+@RequestMapping(value = "users")
+public class UserResource {
+
+	@Autowired
+	private UserService service;
+
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		return ResponseEntity.ok().body(service.findAll());
+	}
+}
---
+++ src/main/java/br/com/mikaelboff/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package br.com.mikaelboff.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.mikaelboff.workshopmongo.domain.User;
+import br.com.mikaelboff.workshopmongo.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
